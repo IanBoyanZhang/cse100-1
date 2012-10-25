0c8618c93df4908fe97fee7a0f1975122b4ec244
Alan Luong <mcluong@acs-cseb220-21.ucsd.edu>
2012-10-24 18:04:32 -0700

    implemented insert function

--- P2/RST.hpp
+++ P2/RST.hpp
@@ -1,12 +1,75 @@
 #ifndef RST_HPP
 #define RST_HPP
 #include "BST.hpp"
+#include "RSTNode.hpp"
 
 template <typename Data>
 class RST : public BST<Data> {
-public:
-  virtual bool insert(const Data& item) {
-    // TODO: implement this function!
-  }
+	public:
+		virtual bool insert(const Data& item) {
+			// TODO: implement this function!
+			RSTNode<Data> *node = new RSTNode<Data>(item);
+			if (this->empty()) {
+				this->root = node;
+				node->parent = 0;
+				++(this->isize);
+				return true;
+			}
+
+			// add node to the correct position
+			BSTNode<Data> *current = this->root;
+			while (current) {
+				// node belongs in left subtree
+				if (item < current->data) {
+					// stop before a null node
+					if (current->left) {
+						current = current->left;
+					} else {
+						// add node to the left
+						node->parent = current;
+						current->left = node;
+						++(this->isize);
+
+						// check priority
+						restore(node);
+
+						return true;
+					}
+					// node belongs in right subtree
+				} else if (current->data < item) {
+					// stop before a null node
+					if (current->right) {
+						current = current->right;
+					} else {
+						// add node to the right
+						node->parent = current;
+						current->right = node;
+						++(this->isize);
+
+						// check priority
+						restore(node);
+
+						return true;
+					}
+				} else {
+					break;
+				}
+			}
+
+			// could not add node
+			return false;
+		}
+
+	private:
+		// check priority and perform necessary rotations
+		void restore(RSTNode<Data>* node) {
+			int priority = node->getPriority();
+			if (priority > ((RSTNode<Data>*)(node->parent))->getPriority()) {
+			}
+		}
+
+		void rotateL(RSTNode<Data>* node) {
+			BSTNode*
+		}
 };
 #endif // RST_HPP
--- P2/RSTNode.hpp
+++ P2/RSTNode.hpp
@@ -1,12 +1,19 @@
 #ifndef RSTNODE_HPP
 #define RSTNODE_HPP
 #include "BSTNode.hpp"
+#include <cstdlib>
 
 template <typename Data>
 class RSTNode : public BSTNode<Data> {
 	public:
-		public RSTNode(Data const & d) : BSTNode<Data>(d) {
+		RSTNode(Data const & d) : BSTNode<Data>(d) {
 			// TODO: implement this constructor!
+			priority = rand();
+		}
+
+		// return the priority
+		int getPriority() {
+			return priority;
 		}
 	private:
 		int priority;
